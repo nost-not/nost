083c93f673da8fa6ab6ba300a8bc7fcfc8fa39c7
gdarquie <gaetan.darquie@gmail.com>
2025-06-09 23:08:06 +0200

    ✨ First mvp: can create an empty  not with correct name and folders

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
+use chrono::Datelike;
+use chrono::Local;
 use dotenv::dotenv;
 use std::env;
 use std::fs::create_dir_all;
 use std::fs::File;
+use std::path::Path;
 
+// todo: give the possibility to create a note for a specific day, eg: 2025-12-31
+// todo: add the templates content to the file
+// todo: add a version number for nost and for not
+// todo: create a repo for not in md
+// todo: correct the tests
 fn main() {
     dotenv().ok();
 
+    // handle pathes
     let not_path = env::var("NOT_PATH").unwrap_or_else(|_| {
         eprintln!("NOT_PATH environment variable not set.");
         Err("NOT_PATH not set").unwrap()
     });
 
-    let not_file_path = compose_file_path();
+    let not_file_path = compose_file_path(&not_path);
     let not_file_name = name_file();
-    let not_file_path_without_title = format!("{}{}", not_path, not_file_path);
-    let full_not_file_path = format!("{}{}", not_file_path_without_title, not_file_name);
+    let full_not_file_path = format!("{}{}", &not_file_path, not_file_name);
 
-    if let Err(e) = create_dir_all(&not_file_path_without_title) {
+    // create folders if needed
+    if let Err(e) = create_dir_all(&not_file_path) {
         eprintln!("Failed to create directory: {}", e);
         return;
     }
 
+    // only create the file if it does not exist
+    if Path::new(&full_not_file_path).exists() {
+        eprintln!(
+            "This note already exists. Nothing has been done.: {}",
+            full_not_file_path
+        );
+        return;
+    }
+
+    // create the file
     match File::create(&full_not_file_path) {
-        Ok(file) => file,
+        Ok(_file) => {
+            println!("File created at: {}", full_not_file_path);
+        }
         Err(e) => {
-            eprintln!("Failed to create file: {}", e);
-            return;
+            eprintln!("Error creating file: {}", e);
         }
     };
 
@@ -33,15 +53,29 @@ fn main() {
 }
 
 fn name_file() -> String {
-    // todo: complete here
-    let file_name = "example.txt";
+    let today = Local::now().date_naive();
+    let day_of_month = today.day();
+    let file_name = format!("{:02}.md", day_of_month);
     file_name.to_string()
 }
 
-fn compose_file_path() -> String {
-    // todo: complete here
-    let file_path = "/temp/";
-    file_path.to_string()
+fn compose_file_path(base_path: &str) -> String {
+    let today = Local::now().date_naive();
+    let year = today.year();
+    let month = format!("{:02}", today.month());
+
+    format!("{}/temp/{}/{}/{}/", base_path, year, month, week_of_month())
+}
+
+fn week_of_month() -> u32 {
+    let today = chrono::Local::now().date_naive();
+    // Get the first day of the month
+    let first_day = today.with_day(1).unwrap();
+    // Calculate the week number for today and the first day of the month
+    let week_today = today.iso_week().week();
+    let week_first = first_day.iso_week().week();
+    // Week of month is difference + 1
+    week_today - week_first + 1
 }
 
 #[cfg(test)]
