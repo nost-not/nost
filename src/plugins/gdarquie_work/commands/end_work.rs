68c83b41fe5b44f0c5d76a72e5b1470a720ea910
gdarquie <gaetan.darquie@gmail.com>
2026-01-06 02:28:00 +0900

    🐛 Work annotations display stop and not start anymore when work ends

--- src/plugins/gdarquie_work/commands/end_work.rs
+++ src/plugins/gdarquie_work/commands/end_work.rs
@@ -16,6 +16,6 @@ pub fn end_work(args: Vec<String>) {
         default_workday = chrono::Local::now().format("%Y-%m-%d").to_string();
         Some(default_workday.as_str())
     };
-    annotate(None, NotEvent::StartWork, None, &not_path, workday);
+    annotate(None, NotEvent::StopWork, None, &not_path, workday);
     std::process::exit(0);
 }
